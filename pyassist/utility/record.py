2cfb089f94ad16a838c0e6f7e610798ee44d7425
Paweł Szumański <36560916+Szumapman@users.noreply.github.com>
2023-12-26 12:09:57 +0100

    Update record.py
    added days_to_birthday() method

--- pyassist/utility/record.py
+++ pyassist/utility/record.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from utility.name import Name
 from utility.phone import Phone
 from utility.email import Email
@@ -45,3 +46,24 @@ class Record:
     def change_email(self, old_email, new_email):
         index = self.emails.index(old_email)
         self.emails[index] = new_email
+        
+    # return amount of days to the next birthday
+    def days_to_birthday(self):
+        current_date = datetime.now().date()
+        this_year_birthday = datetime(
+            year=current_date.year,
+            month=self.birthday.value.month,
+            day=self.birthday.value.day,
+        ).date()
+        difference = this_year_birthday - current_date
+        if difference.days == 0:
+            return f"{self.name}'s birthday is today!"
+        elif difference.days > 0:  # if the birthday is before this year's end
+            return f"day(s) to next birthday: {difference.days}"
+        # if the next birthday is in next year
+        next_birthday = datetime(
+            year=this_year_birthday.year + 1,
+            month=this_year_birthday.month,
+            day=this_year_birthday.day,
+        ).date()
+        return f"day(s) to next birthday: {(next_birthday -  current_date).days}"
