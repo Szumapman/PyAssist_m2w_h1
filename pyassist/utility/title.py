502dc0774cea5c5a4bd71b75f8265cf37dd83c2e
Paweł Szumański <36560916+Szumapman@users.noreply.github.com>
2023-12-29 11:07:26 +0100

    Create title.py

---
+++ pyassist/utility/title.py
@@ -0,0 +1,12 @@
+from utility.field import Field
+
+
+class Title(Field):
+    """
+    class for title object
+
+    Args:
+        Field (class): parent class
+    """
+    def __init__(self, value: str) -> None:
+        self.value = value
