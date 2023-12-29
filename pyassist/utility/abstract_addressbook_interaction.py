311966f727d726db99dccf5146ad6d2b5a313a77
Paweł Szumański <36560916+Szumapman@users.noreply.github.com>
2023-12-29 15:32:26 +0100

    Update abstract_addressbook_interaction.py

--- pyassist/utility/abstract_addressbook_interaction.py
+++ pyassist/utility/abstract_addressbook_interaction.py
@@ -1,36 +1,30 @@
 from abc import abstractmethod, ABC
-from utility.addressbook import AddressBook
-from utility.addressbook import Name
-from utility.phone import Phone
-from utility.email import Email
-from utility.birthday import Birthday
-from utility.address import Address
-from utility.record import Record
+
 
 class AbstractAddressbookInteraction(ABC):
         
     @abstractmethod
-    def add_name(self) -> Name:
+    def add_name(self):
         pass
     
     
     @abstractmethod
-    def add_phone(self) -> Phone:
+    def add_phone(self):
         pass
     
     
     @abstractmethod
-    def add_email(self) -> Email:
+    def add_email(self):
         pass
 
 
     @abstractmethod
-    def add_birthday(self) -> Birthday:
+    def add_birthday(self):
         pass
     
 
     @abstractmethod
-    def add_address(self) -> Address:
+    def add_address(self):
         pass
     
 
@@ -39,7 +33,7 @@ class AbstractAddressbookInteraction(ABC):
         pass    
     
     @abstractmethod
-    def del_record(self) -> Record:
+    def del_record(self):
         pass
     
     
@@ -49,62 +43,62 @@ class AbstractAddressbookInteraction(ABC):
     
     
     @abstractmethod
-    def show_upcoming_birthday(self, *args):
+    def show_upcoming_birthday(self):
         pass
     
     
     @abstractmethod
-    def edit_name(self, record):
+    def edit_name(self):
         pass
     
     
     @abstractmethod
-    def edit_birthday(self, record):
+    def edit_birthday(self):
         pass
     
     
     @abstractmethod
-    def edit_address(self, record):
+    def edit_address(self):
         pass
     
     
     @abstractmethod
-    def edit_phone(self, record):
+    def edit_phone(self):
         pass
     
     
     @abstractmethod
-    def edit_email(email, record):
+    def edit_email(self):
         pass
     
     
     @abstractmethod
-    def edit_record(self, *args):
+    def edit_record(self):
         pass
     
     
     @abstractmethod
-    def search(self, *args):
+    def search(self):
         pass
     
     
     @abstractmethod
-    def export_to_csv(self, *args):
+    def export_to_csv(self):
         pass
     
     
     @abstractmethod
-    def import_from_csv(self, *args):
+    def import_from_csv(self):
         pass
     
     
     @abstractmethod
-    def save_addressbook(self, filename):
+    def save_addressbook(self):
         pass
     
     
     @abstractmethod
-    def load_addressbook(self, filename):
+    def load_addressbook(self):
         pass
     
     
